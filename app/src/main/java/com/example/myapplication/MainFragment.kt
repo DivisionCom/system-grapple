74b5e4ce4789318b224674b62d6b50f921698afa
DivisionCom <xd6511@gmail.com>
2023-12-24 22:03:19 +0300

    Add viewBinding on Fragment

--- app/src/main/java/com/example/myapplication/MainFragment.kt
+++ app/src/main/java/com/example/myapplication/MainFragment.kt
@@ -5,14 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.myapplication.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
