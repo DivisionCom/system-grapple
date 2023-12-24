5024b0cecf6389406d2a7931a862e571da3ecd0f
DivisionCom <xd6511@gmail.com>
2023-12-24 23:13:53 +0300

    Add styles: colors, fonts, background

--- app/src/main/java/com/example/myapplication/MenuFragment.kt
+++ app/src/main/java/com/example/myapplication/MenuFragment.kt
@@ -13,7 +13,7 @@ class MenuFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentMenuBinding.inflate(inflater, container, false)
         return binding.root
     }
